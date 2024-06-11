69160ea969b4062413314372e8c4492f4e9740d3
sebil1993 <sebilerche@gmx.net>
2024-06-11 17:38:10 +0200

    add button press

--- src/output_read_von_drehknopf_button_press.ino
+++ src/output_read_von_drehknopf_button_press.ino
@@ -1,10 +1,19 @@
 
 #include <Arduino.h>
 
-const byte pin1 = 5; // Connected to DT on KY-040
-const byte pin2 = 4; // Connected to CLK on KY-040
+const int pin1 = 5;
+const int pin2 = 4;
+const int keyPin = 0;
+
 int state = 0;
 int counter = 0;
+int pressCounter = 0;
+
+unsigned long debounceDelay = 100;
+unsigned long lastDebounce = 0;
+
+bool buttonState = HIGH;
+bool lastButtonState = HIGH;
 
 const unsigned char ttable[7][4] = {
     {0, 2, 4, 0},
@@ -20,6 +29,7 @@ void setup()
 {
   pinMode(pin1, INPUT_PULLUP);
   pinMode(pin2, INPUT_PULLUP);
+  pinMode(keyPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
@@ -27,7 +37,6 @@ void loop()
 {
 
   unsigned char result = process();
-
   // Clockwise rotation.
   if (result == 16)
   {
@@ -41,6 +50,26 @@ void loop()
     counter--;
     Serial.println(counter);
   }
+
+  bool reading = digitalRead(keyPin);
+  if (reading != lastButtonState)
+  {
+    lastDebounce = millis();
+  }
+  if ((millis() - lastDebounce) > debounceDelay)
+  {
+    if (reading != buttonState)
+    {
+      buttonState = reading;
+      if (buttonState == LOW)
+      {
+        pressCounter++;
+        Serial.print("Press: ");
+        Serial.println(pressCounter);
+      }
+    }
+  }
+  lastButtonState = reading;
 }
 
 unsigned char process()
@@ -54,4 +83,14 @@ unsigned char process()
 
   // Return emit bits, ie the generated event.
   return state & 48;
+}
+
+bool processButton(bool btn)
+{
+  if (btn == true)
+  {
+    return false;
+  }
+  else
+    return true;
 }
